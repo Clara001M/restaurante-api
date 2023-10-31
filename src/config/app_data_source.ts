dab0d8eabb436fed86faafa877384b2604f4e583
Clara001M <138584425+Clara001M@users.noreply.github.com>
2023-10-31 10:23:22 -0300

    configurando cadastroRestaurante (#4)
    * configurando cadastroRestaurante
    
    * back-end cadastro do restaurante
    
    ---------
    
    Co-authored-by: Giordano <dgiordanobruno@gmail.com>

--- src/config/app_data_source.ts
+++ src/config/app_data_source.ts
@@ -2,6 +2,8 @@ import { DataSource } from 'typeorm'
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
+  database: './database.sqlite',
+  entities: ['./src/models/*.{ts,js}'],
+  synchronize: true
   database: './crudtable.sqlite'
-
 })
